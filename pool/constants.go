18c682d7d5eb2d6c1dbdc8e59a5abc1aa4a66b20
jxsl13 <jxsl13@googlemail.com>
2022-10-05 18:15:23 +0200

    add dead letter exchange constant

--- pool/constants.go
+++ pool/constants.go
@@ -6,3 +6,9 @@ var (
 		"x-queue-type": "quorum",
 	}
 )
+
+const (
+	// DeadLetterExchangeKey can be used in order to create a dead letter exchange
+	// https://www.rabbitmq.com/dlx.html
+	DeadLetterExchangeKey = "x-dead-letter-exchange"
+)
