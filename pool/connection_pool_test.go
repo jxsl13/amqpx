f886661c3718679523894931633ae0bab00adc8d
jxsl13 <jxsl13@gmail.com>
2022-10-08 12:10:37 +0200

    add connection pool disconnect test

--- pool/connection_pool_test.go
+++ pool/connection_pool_test.go
@@ -39,3 +39,41 @@ func TestNewConnectionPool(t *testing.T) {
 
 	wg.Wait()
 }
+
+func TestNewConnectionPoolDisconnect(t *testing.T) {
+	connections := 100 // do not go below 100
+	p, err := pool.NewConnectionPool("amqp://admin:password@localhost:5672", connections,
+		pool.ConnectionPoolWithName("TestNewConnectionPoolDisconnect"),
+		pool.ConnectionPoolWithLogger(logging.NewTestLogger(t)),
+	)
+	if err != nil {
+		assert.NoError(t, err)
+		return
+	}
+	defer p.Close()
+	var wg sync.WaitGroup
+
+	awaitStarted, awaitStopped := DisconnectWithStartStop(t, 0, time.Duration(connections)*10*time.Millisecond, 2*time.Second)
+	defer awaitStopped()
+
+	for i := 0; i < connections; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+
+			awaitStarted() // wait for connection loss
+
+			// no connection, this should retry until there is a connection
+			c, err := p.GetConnection()
+			if err != nil {
+				assert.NoError(t, err)
+				return
+			}
+
+			time.Sleep(1 * time.Second)
+			p.ReturnConnection(c, false)
+		}(i)
+	}
+
+	wg.Wait()
+}
