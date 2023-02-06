3b76a2d008550e1fa034a65180ae6638e292ac2f
jxsl13 <jxsl13@googlemail.com>
2023-02-06 19:47:35 +0100

    wrap underlying amqp091 errors & improve recoverability

--- pool/errors.go
+++ pool/errors.go
@@ -1,10 +1,18 @@
 package pool
 
-import "errors"
+import (
+	"errors"
+	"net"
+
+	"github.com/rabbitmq/amqp091-go"
+)
 
 var (
 	ErrInvalidConnectURL = errors.New("invalid connection url")
-	ErrConnectionFailed  = errors.New("connection failed")
+
+	// ErrConnectionFailed is just a generic error that is not checked
+	// explicitly against in the code.
+	ErrConnectionFailed = errors.New("connection failed")
 
 	errInvalidPoolSize          = errors.New("invalid pool size")
 	ErrPoolInitializationFailed = errors.New("pool initialization failed")
@@ -24,3 +32,35 @@ var (
 
 	ErrDeliveryClosed = errors.New("delivery channel closed")
 )
+
+func recoverable(err error) bool {
+	if err == nil {
+		panic("checking nil error for recoverability")
+	}
+
+	oe := &net.OpError{}
+	if errors.As(err, &oe) {
+		return true
+	}
+
+	ae := &amqp091.Error{}
+	switch {
+	case errors.As(err, &ae), errors.As(err, ae):
+		switch ae.Code {
+		case notImplemented:
+			return false
+		default:
+			// recoverability according to amqp091 is when
+			// the result can be changing by changing use rinput.
+
+			// recoverability according to this library is
+			// changing the result by reconnecting
+
+			// not recoverable by changing user input
+			// because of potential connection loss
+			return !ae.Recover
+		}
+	}
+
+	return errors.Is(err, amqp091.ErrClosed)
+}
