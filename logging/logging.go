c827aaf8b737fc761ca626a65800fb254178da65
jxsl13 <jxsl13@googlemail.com>
2023-02-08 10:39:39 +0100

    shrink logger interface

--- logging/logging.go
+++ logging/logging.go
@@ -13,17 +13,11 @@ type Logger interface {
 type BasicLogger interface {
 	Debugf(format string, args ...any)
 	Infof(format string, args ...any)
-	Printf(format string, args ...any)
 	Warnf(format string, args ...any)
 	Errorf(format string, args ...any)
-	Fatalf(format string, args ...any)
-	Panicf(format string, args ...any)
 
 	Debug(args ...any)
 	Info(args ...any)
-	Print(args ...any)
 	Warn(args ...any)
 	Error(args ...any)
-	Fatal(args ...any)
-	Panic(args ...any)
 }
