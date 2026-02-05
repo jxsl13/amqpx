jxsl13/amqpx

commit eb0745afb2ecf74debd5ba25a8eb4a412cba16de

rename *Size methods to *Capacity and make Size methods represent the current number of idle connections or sessions on the pools

pool/pool.go
@@ -33,9 +33,9 @@ func New(ctx context.Context, connectUrl string, numConns, numSessions int, opti
 	// use sane defaults
 	option := poolOption{
 		cpo: connectionPoolOption{
-			Name: defaultAppName(),
-			Ctx:  ctx,
-			Size: numConns, // at least one connection
+			Name:     defaultAppName(),
+			Ctx:      ctx,
+			Capacity: numConns, // at least one connection
 
 			ConnHeartbeatInterval: 15 * time.Second,
 			ConnTimeout:           30 * time.Second,
@@ -44,9 +44,9 @@ func New(ctx context.Context, connectUrl string, numConns, numSessions int, opti
 			Logger: logger,
 		},
 		spo: sessionPoolOption{
-			Size:        numSessions,
-			Confirmable: false, // require publish confirmations
-			BufferSize:  1,     // fault tolerance over throughput
+			Capacity:       numSessions,
+			Confirmable:    true, // require publish confirmations
+			BufferCapacity: 10,
 
 			Logger: logger,
 		},
@@ -106,10 +106,22 @@ func (p *Pool) Name() string {
 	return p.cp.Name()
 }
 
-func (p *Pool) ConnectionPoolSize() int {
-	return p.cp.Size()
+// ConnectionPoolCapacity returns the capacity of the connection pool.
+func (p *Pool) ConnectionPoolCapacity() int {
+	return p.cp.Capacity()
 }
 
+// ConnectionPoolSize returns the number of connections in the pool that are idling.
+func (p *Pool) ConnectionPoolSize() int {
+	return p.cp.Capacity()
+}
+
+// SessionPoolCapacity returns the capacity of the session pool.
+func (p *Pool) SessionPoolCapacity() int {
+	return p.sp.Capacity()
+}
+
+// SessionPoolSize returns the number of sessions in the pool that are idling.
 func (p *Pool) SessionPoolSize() int {
 	return p.sp.Size()
 }

amqpx_options.go
@@ -57,11 +57,11 @@ func WithTLS(config *tls.Config) Option {
 	}
 }
 
-// WithBufferSize allows to configurethe size of
+// WithBufferCapacity allows to configurethe size of
 // the confirmation, error & blocker buffers of all sessions
-func WithBufferSize(size int) Option {
+func WithBufferCapacity(capacity int) Option {
 	return func(o *option) {
-		o.PoolOptions = append(o.PoolOptions, pool.WithBufferSize(size))
+		o.PoolOptions = append(o.PoolOptions, pool.WithBufferCapacity(capacity))
 	}
 }
 

pool/connection_pool.go
@@ -22,7 +22,7 @@ type ConnectionPool struct {
 	heartbeat   time.Duration
 	connTimeout time.Duration
 
-	size int
+	capacity int
 
 	tls *tls.Config
 
@@ -49,8 +49,8 @@ func NewConnectionPool(ctx context.Context, connectUrl string, numConns int, opt
 
 	// use sane defaults
 	option := connectionPoolOption{
-		Name: defaultAppName(),
-		Size: numConns,
+		Name:     defaultAppName(),
+		Capacity: numConns,
 
 		Ctx: ctx,
 
@@ -92,9 +92,9 @@ func newConnectionPoolFromOption(connectUrl string, option connectionPoolOption)
 		heartbeat:   option.ConnHeartbeatInterval,
 		connTimeout: option.ConnTimeout,
 
-		size:        option.Size,
+		capacity:    option.Capacity,
 		tls:         option.TLSConfig,
-		connections: make(chan *Connection, option.Size),
+		connections: make(chan *Connection, option.Capacity),
 
 		ctx:    ctx,
 		cancel: cancel,
@@ -122,7 +122,7 @@ func newConnectionPoolFromOption(connectUrl string, option connectionPoolOption)
 }
 
 func (cp *ConnectionPool) initCachedConns() error {
-	for id := int64(0); id < int64(cp.size); id++ {
+	for id := int64(0); id < int64(cp.capacity); id++ {
 		conn, err := cp.deriveConnection(cp.ctx, id, true)
 		if err != nil {
 			return fmt.Errorf("%w: %v", ErrPoolInitializationFailed, err)
@@ -234,10 +234,10 @@ func (cp *ConnectionPool) Close() {
 	defer cp.info("closed")
 
 	wg := &sync.WaitGroup{}
-	wg.Add(cp.size)
+	wg.Add(cp.capacity)
 	cp.cancel()
 
-	for i := 0; i < cp.size; i++ {
+	for i := 0; i < cp.capacity; i++ {
 		go func() {
 			defer wg.Done()
 			conn := <-cp.connections
@@ -255,19 +255,20 @@ func (cp *ConnectionPool) StatTransientActive() int {
 	return cp.concurrentTransient
 }
 
-// StatCachedIdle returns the number of idle cached connections.
-func (cp *ConnectionPool) StatCachedIdle() int {
+// StatCachedActive returns the number of active cached connections.
+func (cp *ConnectionPool) StatCachedActive() int {
+	return cp.capacity - len(cp.connections)
+}
+
+// Size returns the number of idle cached connections.
+func (cp *ConnectionPool) Size() int {
 	return len(cp.connections)
 }
 
-// StatCachedActive returns the number of active cached connections.
-func (cp *ConnectionPool) StatCachedActive() int {
-	return cp.size - len(cp.connections)
-}
-
-// Size is the total size of the cached connection pool without any transient connections.
-func (cp *ConnectionPool) Size() int {
-	return cp.size
+// Capacity is the capacity of the cached connection pool without any transient connections.
+// It is the initial number of connections that were created for this connection pool.
+func (cp *ConnectionPool) Capacity() int {
+	return cp.capacity
 }
 
 func (cp *ConnectionPool) catchShutdown() <-chan struct{} {

pool/connection_pool_options.go
@@ -17,7 +17,7 @@ type connectionPoolOption struct {
 	Name string
 	Ctx  context.Context
 
-	Size int
+	Capacity int
 
 	ConnHeartbeatInterval time.Duration
 	ConnTimeout           time.Duration

pool/pool_options.go
@@ -67,11 +67,11 @@ func WithTLS(config *tls.Config) Option {
 	}
 }
 
-// WithBufferSize allows to configurethe size of
+// WithBufferCapacity allows to configurethe size of
 // the confirmation, error & blocker buffers of all sessions
-func WithBufferSize(size int) Option {
+func WithBufferCapacity(size int) Option {
 	return func(po *poolOption) {
-		SessionPoolWithBufferSize(size)(&po.spo)
+		SessionPoolWithBufferCapacity(size)(&po.spo)
 	}
 }
 

pool/session.go
@@ -17,11 +17,11 @@ const (
 
 // Session is
 type Session struct {
-	name        string
-	cached      bool
-	flagged     bool
-	confirmable bool
-	bufferSize  int
+	name           string
+	cached         bool
+	flagged        bool
+	confirmable    bool
+	bufferCapacity int
 
 	channel  *amqp091.Channel
 	returned chan amqp091.Return
@@ -83,10 +83,10 @@ func NewSession(conn *Connection, name string, options ...SessionOption) (*Sessi
 
 	// default values
 	option := sessionOption{
-		Logger:      conn.log, // derive logger form connection
-		Cached:      false,
-		Confirmable: false,
-		BufferSize:  100,
+		Logger:         conn.log, // derive logger form connection
+		Cached:         false,
+		Confirmable:    false,
+		BufferCapacity: 10,
 		// derive context from connection, as we are derived from the connection
 		// so in case the connection is closed, we are closed as well.
 		Ctx:           conn.ctx,
@@ -102,10 +102,10 @@ func NewSession(conn *Connection, name string, options ...SessionOption) (*Sessi
 	cancel := toCancelFunc(fmt.Errorf("session %w", ErrClosed), cc)
 
 	session := &Session{
-		name:        name,
-		cached:      option.Cached,
-		confirmable: option.Confirmable,
-		bufferSize:  option.BufferSize,
+		name:           name,
+		cached:         option.Cached,
+		confirmable:    option.Confirmable,
+		bufferCapacity: option.BufferCapacity,
 
 		consumers: map[string]bool{},
 		channel:   nil, // will be created on connect
@@ -261,18 +261,18 @@ func (s *Session) connect() (err error) {
 		return fmt.Errorf("%v: %w", ErrConnectionFailed, err)
 	}
 
-	s.errors = make(chan *amqp091.Error, s.bufferSize)
+	s.errors = make(chan *amqp091.Error, s.bufferCapacity)
 	channel.NotifyClose(s.errors)
 
 	if s.confirmable {
-		s.confirms = make(chan amqp091.Confirmation, s.bufferSize)
+		s.confirms = make(chan amqp091.Confirmation, s.bufferCapacity)
 		channel.NotifyPublish(s.confirms)
 		err = channel.Confirm(false)
 		if err != nil {
 			return err
 		}
 
-		s.returned = make(chan amqp091.Return, s.bufferSize)
+		s.returned = make(chan amqp091.Return, s.bufferCapacity)
 		channel.NotifyReturn(s.returned)
 	}
 

pool/session_options.go
@@ -7,12 +7,12 @@ import (
 )
 
 type sessionOption struct {
-	Logger        logging.Logger
-	Cached        bool
-	Confirmable   bool
-	BufferSize    int
-	Ctx           context.Context
-	AutoCloseConn bool
+	Logger         logging.Logger
+	Cached         bool
+	Confirmable    bool
+	BufferCapacity int
+	Ctx            context.Context
+	AutoCloseConn  bool
 
 	RecoverCallback                     SessionRetryCallback
 	PublishRetryCallback                SessionRetryCallback
@@ -72,9 +72,9 @@ func SessionWithConfirms(requiresPublishConfirms bool) SessionOption {
 
 // SessionWithBufferSize allows to customize the size of th einternal channel buffers.
 // all buffers/channels are initialized with this size. (e.g. error or confirm channels)
-func SessionWithBufferSize(size int) SessionOption {
+func SessionWithBufferCapacity(capacity int) SessionOption {
 	return func(so *sessionOption) {
-		so.BufferSize = size
+		so.BufferCapacity = capacity
 	}
 }
 

pool/session_pool.go
@@ -15,10 +15,10 @@ type SessionPool struct {
 
 	transientID int64
 
-	size        int
-	bufferSize  int
-	confirmable bool
-	sessions    chan *Session
+	capacity       int
+	bufferCapacity int
+	confirmable    bool
+	sessions       chan *Session
 
 	ctx    context.Context
 	cancel context.CancelFunc
@@ -52,11 +52,11 @@ func NewSessionPool(pool *ConnectionPool, numSessions int, options ...SessionPoo
 
 	// use sane defaults
 	option := sessionPoolOption{
-		AutoClosePool: false, // caller owns the connection pool by default
-		Size:          numSessions,
-		Confirmable:   false,
-		BufferSize:    1,        // fault tolerance over throughput
-		Logger:        pool.log, // derive logger from connection pool
+		AutoClosePool:  false, // caller owns the connection pool by default
+		Capacity:       numSessions,
+		Confirmable:    false,
+		BufferCapacity: 10,       // fault tolerance over throughput
+		Logger:         pool.log, // derive logger from connection pool
 	}
 
 	for _, o := range options {
@@ -76,10 +76,10 @@ func newSessionPoolFromOption(pool *ConnectionPool, ctx context.Context, option 
 		pool:              pool,
 		autoCloseConnPool: option.AutoClosePool,
 
-		size:        option.Size,
-		bufferSize:  option.BufferSize,
-		confirmable: option.Confirmable,
-		sessions:    make(chan *Session, option.Size),
+		capacity:       option.Capacity,
+		bufferCapacity: option.BufferCapacity,
+		confirmable:    option.Confirmable,
+		sessions:       make(chan *Session, option.Capacity),
 
 		ctx:    ctx,
 		cancel: cancel,
@@ -124,7 +124,7 @@ func newSessionPoolFromOption(pool *ConnectionPool, ctx context.Context, option 
 }
 
 func (sp *SessionPool) initCachedSessions() error {
-	for i := 0; i < sp.size; i++ {
+	for i := 0; i < sp.capacity; i++ {
 		session, err := sp.initCachedSession(i)
 		if err != nil {
 			return err
@@ -157,9 +157,14 @@ func (sp *SessionPool) initCachedSession(id int) (*Session, error) {
 	}
 }
 
-// Size returns the size of the session pool which indicate sthe number of available cached sessions.
+// Size returns the number of available idle sessions in the pool.
 func (sp *SessionPool) Size() int {
-	return sp.size
+	return len(sp.sessions)
+}
+
+// Capacity returns the size of the session pool which indicate t he number of available cached sessions.
+func (sp *SessionPool) Capacity() int {
+	return sp.capacity
 }
 
 // GetSession gets a pooled session.
@@ -209,7 +214,7 @@ func (sp *SessionPool) deriveSession(ctx context.Context, conn *Connection, id i
 
 	return NewSession(conn, name,
 		SessionWithContext(ctx),
-		SessionWithBufferSize(sp.size),
+		SessionWithBufferCapacity(sp.bufferCapacity),
 		SessionWithCached(cached),
 		SessionWithConfirms(sp.confirmable),
 		SessionWithAutoCloseConnection(!cached), // only close transient connections
@@ -280,7 +285,7 @@ func (sp *SessionPool) Close() {
 	wg := &sync.WaitGroup{}
 
 	// close all sessions:
-	for i := 0; i < sp.size; i++ {
+	for i := 0; i < sp.capacity; i++ {
 		session := <-sp.sessions
 		wg.Add(1)
 		go func(s *Session) {

commit e54bc71448305340f515b2d839b6bd4863a02f2f

Make Pool.ConnectionPoolSize() return the right size
Commit eb0745a introduced a bug in Pool where
ConnectionPoolCapacity() and .ConnectionPoolSize() both returned
the pool's capacity.

pool/pool.go
@@ -124,7 +124,7 @@ func (p *Pool) ConnectionPoolCapacity() int {
 
 // ConnectionPoolSize returns the number of connections in the pool that are idling.
 func (p *Pool) ConnectionPoolSize() int {
-	return p.cp.Capacity()
+	return p.cp.Size()
 }
 
 // SessionPoolCapacity returns the capacity of the session pool.